7afec0b4203739f6aefba67a7c85b9c37936f147
Hudson-H <hudsongenteboa70@gmail.com>
2025-03-25 21:16:49 -0300

    fix: arruma string que é usada para ser o nome da imagem final editada

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ for campo, coordenadas_campo in csv_data_processor.img_data_coord.items():
     for pos in coordenadas_campo:
         draw.text(pos, campo, fill="black", font=font)
  
-file_name = img_path.strip('/')[-1]
+file_name = img_path.split('/')[-1]
 
-imagem.save(f"../io/filled_records/{file_name}.png")
+imagem.save(f"../io/filled_records/{file_name.removesuffix(".png")}.png")
 imagem.show()
